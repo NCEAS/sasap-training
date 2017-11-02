8f4f36713604bf178dea0af926b29de04eb07307
Jeanette <jclark@nceas.ucsb.edu>
2017-11-02 15:28:16 -0700

    fixed paths and column names

--- materials/reproducible-analysis-in-r/data/data_formatting_scripts/BristolBay_formatting.R
+++ materials/reproducible-analysis-in-r/data/data_formatting_scripts/BristolBay_formatting.R
@@ -22,7 +22,7 @@ reformat_bristolbay <- function(sheetnumber, stock.ID, stock, Sub.Region){
 
 
     colnames(b) <- c("BroodYear", 'R0.1',"R0.2","R0.3","R0.4","R0.5", "R1.1","R1.2",'R1.3',"R1.4","R1.5","R2.1",
-                     'R2.2',"R2.3", 'R2.4',"R3.1", "R3.2", "R3.3","R3.4", "Escapement", "Stock.ID","Species",'Stock',"Region","Sub.Region","UseFlag")
+                     'R2.2',"R2.3", 'R2.4',"R3.1", "R3.2", "R3.3","R3.4", "TotalEscapement", "Stock.ID","Species",'Stock',"Region","Sub.Region","UseFlag")
 
 
 
