56760591acd72d39977fcca7c64ff50269be86bb
Jeanette <jclark@nceas.ucsb.edu>
2017-11-02 15:30:44 -0700

    fixed another path

--- materials/reproducible-analysis-in-r/data/data_formatting_scripts/Eshamy_formatting.R
+++ materials/reproducible-analysis-in-r/data/data_formatting_scripts/Eshamy_formatting.R
@@ -1,6 +1,6 @@
 #Reformatting Eshamy brood table data
 
-b <- read.csv('Original/Eshamy_Sockeye.csv', stringsAsFactors = F)
+b <- read.csv('original/Eshamy_Sockeye.csv', stringsAsFactors = F)
 #convert to numeric
 
 b <- lapply(b, as.numeric)
@@ -16,7 +16,7 @@ b$Region <- 'PWS'
 b$Sub.Region <- 'PWS'
 b$Use <- 1
 
-colnames(b) <- c("BroodYear", "TotalEscapement","R0.1","R0.2","R1.1","R1.2","R2.1","R1.3","R2.2",  
+colnames(b) <- c("BroodYear", "TotalEscapement","R0.1","R0.2","R1.1","R1.2","R2.1","R1.3","R2.2",
                  "R2.3","R3.2","Stock.ID","Species",'Stock',"Region","Sub.Region","UseFlag")
 #find where there are no real escapement values and write Use == 0 for these rows
 #most of these will be no weir data
@@ -36,7 +36,7 @@ b$R3.1 <- 0; b$R3.3<- 0; b$R3.4 <- 0
 #put columns in the correct order for the sheet
 b <- b[, c('Stock.ID', 'Species', 'Stock','Region','Sub.Region', 'UseFlag',
            'BroodYear','TotalEscapement','R0.1','R0.2', 'R0.3',	'R0.4',	'R0.5',
-           'R1.1','R1.2','R1.3','R1.4',	'R1.5', 
+           'R1.1','R1.2','R1.3','R1.4',	'R1.5',
            'R2.1','R2.2','R2.3','R2.4',
            'R3.1','R3.2','R3.3','R3.4')]
 
