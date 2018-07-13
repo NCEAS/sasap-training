834cb53fe1001f984272f2469ad5d039a1e9d705
Jeanette Clark <jclark@nceas.ucsb.edu>
2018-07-13 11:19:21 -0700

    more path wrangling

--- _build_and_merge.R
+++ _build_and_merge.R
@@ -4,6 +4,12 @@ library(git2r)
 tag_list <- names(tags())
 dir_names <- c("reproducible-research-in-r-juneau", "reproducible-research-in-r-anchorage")
 
+for (n in dir_names){
+  if (dir.exists(paste0("public/materials/", n)) == FALSE){
+    dir.create(paste0("public/materials/", n), recursive = T)
+  }
+}
+
 # Build all books in the books subdir
 for (zz in 1:length(tag_list)) {
 
@@ -14,15 +20,15 @@ for (zz in 1:length(tag_list)) {
   devtools::install_deps('.') # Installs book-specific R deps
   # defined in DESCRIPTION file
   bookdown::render_book('index.Rmd', c('bookdown::gitbook'))
-
-  if (dir.exists(paste0("../../public/materials/", dir_names[zz])) == FALSE){
-    dir.create(paste0("../../public/materials/", dir_names[zz]), recursive = T)
-  }
+  #Rscript -e "bookdown::render_book('index.Rmd', c('bookdown::gitbook', 'bookdown::pdf_book', 'bookdown::epub_book'))"
+  setwd(".."); setwd("..")
 
   fls <- list.files("_book")
 
   file.copy(paste0("_book/",fls), paste0("../../public/materials/", dir_names[zz]), recursive = T, overwrite = T, copy.mode = T)
 
   unlink("_book", recursive = T)
+
+  setwd("../..")
 }
 
