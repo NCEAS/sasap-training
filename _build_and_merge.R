eabc798f37049d8a547b6be914ad00dd0d66460a
Jeanette Clark <jclark@nceas.ucsb.edu>
2018-07-13 11:01:58 -0700

    fixed directory copying business

--- _build_and_merge.R
+++ _build_and_merge.R
@@ -4,23 +4,29 @@ library(git2r)
 tag_list <- names(tags())
 dir_names <- c("reproducible-research-in-r-juneau", "reproducible-research-in-r-anchorage")
 
-if (dir.exists("public/materials") == FALSE){
-  dir.create("public/materials", recursive = T)
+for (n in dir_names){
+  if (dir.exists(paste0("public/materials/", n)) == FALSE){
+    dir.create(paste0("public/materials/", n), recursive = T)
+  }
 }
 
 # Build all books in the books subdir
-for (i in 1:length(tag_list)) {
+for (zz in 1:length(tag_list)) {
 
-  print(paste("Building book ", tag_list[i]))
-  checkout(".", tag_list[i])
+  print(paste("Building book ", tag_list[zz]))
+  checkout(".", tag_list[zz])
+
   setwd("materials/reproducible-analysis-in-r")
   devtools::install_deps('.') # Installs book-specific R deps
   # defined in DESCRIPTION file
   bookdown::render_book('index.Rmd', c('bookdown::gitbook'))
   #Rscript -e "bookdown::render_book('index.Rmd', c('bookdown::gitbook', 'bookdown::pdf_book', 'bookdown::epub_book'))"
-  file.copy("_book", file.path("/public/materials/", dir_names[i]), recursive = T)
-  file.remove("_book", recursive = T)
+  setwd(".."); setwd("..")
+
+  fls <- list.files("_book")
+
+  file.copy(paste0("_book/",fls), paste0("../../public/materials/", dir_names[zz]), recursive = T, overwrite = T, copy.mode = T)
+
   unlink("_book", recursive = T)
 }
-
 
